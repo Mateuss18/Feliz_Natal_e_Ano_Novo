c55d6121d06c2ca8154d25650c175b9ab772a4b2
Mateuss18 <mateusdfl18@gmail.com>
2021-12-29 20:08:00 -0300

    Animações de scroll

---
+++ script.js
@@ -0,0 +1,13 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '10px',
+    duration: 2000,
+    reset: false
+});
+    
+ScrollReveal().reveal('.mensagem', { delay: 300 });
+ScrollReveal().reveal('.botao', { delay: 300 });
+ScrollReveal().reveal('aside', { delay: 300 });
+ScrollReveal().reveal('#titulo', { delay: 1000 });
+ScrollReveal().reveal('.cardAcessorio', { interval: 300 });
+ScrollReveal().reveal('#imagem', { delay: 300 });
